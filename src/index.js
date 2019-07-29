7a3ab332b793bc89a1ae429d9a1c8daa8c6274f7
paulotijero <paulotijero@gmail.com>
2019-07-28 20:45:49 -0500

    Add emotion core and global style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import { Global } from "@emotion/core";
+
+const global = {
+  body: {
+    margin: 0,
+    fontFamily: "'Roboto', sans-serif"
+  }
+};
 
 function App() {
-  return <h1>Init project</h1>;
+  return (
+    <>
+      <Global styles={global} />
+      <h1>Init project</h1>
+    </>
+  );
 }
 
 const $root = document.getElementById("root");
